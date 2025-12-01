6775944d05a28454ad0eb3e79231c98c7a4148cd
elkomy2055 <abdoelkomy2055@gmail.com>
2025-12-01 19:37:41 +0200

    Delete backend/src/time-mangment directory

--- backend/src/time-mangment/model/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './shift.schema';
-export * from './attendance.schema';
-export * from './overtime.schema';
-export * from './attendance-correction.schema';
-export * from './employee-shift-assignment.schema';
-export * from './holiday-calendar.schema';
-export * from './rest-day.schema';
--- backend/src/time-mangment/time-mangment.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TimeMangmentService {
-  getHealth(): string {
-    return 'Time management module is healthy';
-  }
-}
-
